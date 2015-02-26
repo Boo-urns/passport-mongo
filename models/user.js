dd29727f9df51f47c87339a39cc2151741588eae
Boo-urns <coreyb@simantel.com>
2015-02-26 07:56:40 -0600

    Setting up user as a schema and removing last name

--- models/user.js
+++ models/user.js
@@ -1,11 +1,25 @@
-
 var mongoose = require('mongoose');
+var Schema 	 = mongoose.Schema;
+var bcrypt	 = require('bcrypt-nodejs');
 
-module.exports = mongoose.model('User',{
-	id: String,
-	username: String,
-	password: String,
-	email: String,
+// user schema
+var UserSchema = new Schema({
 	firstName: String,
-	lastName: String
+	username: { type: String, required: true, index: { unique: true}},
+	password: { type: String, required: true },
+	email: { type: String, required: true},
+	registered: { type: Date, default: Date.now }
 });
+
+
+// return the model
+module.exports = mongoose.model('User', UserSchema);
+
+// module.exports = mongoose.model('User',{
+// 	id: String,
+// 	username: String,
+// 	password: String,
+// 	email: String,
+// 	firstName: String,
+// 	lastName: String
+// });
